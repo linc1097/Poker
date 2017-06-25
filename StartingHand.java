200790281a2bb9625577c90b6cc13ba27af3f496
Lincoln Updike <linc1097@gmail.com>
2017-06-24 21:26:48 -0500

    with Starting Hand class

---
+++ StartingHand.java
@@ -0,0 +1,59 @@
+
+/**
+ * Write a description of class StartingHand here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class StartingHand
+{
+    private int rank1;
+    private int rank2;
+    private boolean isSuited;
+
+    public StartingHand(int r1, int r2, boolean suited)
+    {
+        if (r1 > r2)
+        {
+            rank1 = r1;
+            rank2 = r2;
+        }
+        else
+        {
+            rank1 = r2;
+            rank2 = r1;
+        }
+        isSuited = suited;
+    }
+
+    public int getRank1()
+    {
+        return rank1;
+    }
+
+    public int getRank2()
+    {
+        return rank2;
+    }
+
+    public boolean getSuited()
+    {
+        return isSuited;
+    }
+
+    public boolean equals(Object x)
+    {
+        StartingHand other = (StartingHand)x;
+        return (rank1 == other.getRank1()) && (rank2 == other.getRank2()) && (isSuited == other.getSuited());
+    }
+
+    public int hashCode()
+    {
+        int x;
+        if (isSuited)
+            x = 256;
+        else
+            x = 0;
+        return x + rank2*16 + rank1;
+    }
+}
